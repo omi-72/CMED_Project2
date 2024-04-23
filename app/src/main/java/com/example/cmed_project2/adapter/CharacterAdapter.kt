bd147bc888756a06eda8c11320a140037526aeca
omi-72 <omi@techetronventures.com>
2024-04-23 15:34:23 +0600

    Create an adapter to bind the list of characters to the RecyclerView

--- app/src/main/java/com/example/cmed_project2/adapter/CharacterAdapter.kt
+++ app/src/main/java/com/example/cmed_project2/adapter/CharacterAdapter.kt
@@ -1,4 +1,59 @@
 package com.example.cmed_project2.adapter
 
-class CharacterAdapter {
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.example.cmed_project2.databinding.ListItemCharacterBinding
+import com.squareup.picasso.Picasso
+
+class CharacterAdapter : ListAdapter<com.example.cmed_project2.model.Character, CharacterAdapter.CharacterViewHolder>(CharacterDiffCallback()) {
+
+    private var onItemClickListener: ((com.example.cmed_project2.model.Character) -> Unit)? = null
+
+    fun setOnItemClickListener(listener: (com.example.cmed_project2.model.Character) -> Unit) {
+        onItemClickListener = listener
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CharacterViewHolder {
+
+        val inflater = LayoutInflater.from(parent.context)
+        val binding = ListItemCharacterBinding.inflate(inflater, parent, false)
+        return CharacterViewHolder(binding)
+
+
+    }
+
+    override fun onBindViewHolder(holder: CharacterViewHolder, position: Int) {
+        holder.bind(getItem(position))
+    }
+
+    inner class CharacterViewHolder(private val binding: ListItemCharacterBinding) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(character: com.example.cmed_project2.model.Character) {
+            binding.firstName.text = character.firstName
+            binding.lastName.text = character.lastName
+            binding.mobileNumber.text = character.mobileNumber
+            Picasso.get().load(character.profileImage).into(binding.profileImage)
+
+        }
+        init {
+            binding.root.setOnClickListener {
+                val position = adapterPosition
+                if (position != RecyclerView.NO_POSITION) {
+                    onItemClickListener?.invoke(getItem(position))
+                }
+            }
+        }
+    }
+}
+
+class CharacterDiffCallback : DiffUtil.ItemCallback<com.example.cmed_project2.model.Character>() {
+    override fun areItemsTheSame(oldItem: com.example.cmed_project2.model.Character, newItem: com.example.cmed_project2.model.Character): Boolean {
+        return oldItem.firstName == newItem.firstName
+    }
+
+    override fun areContentsTheSame(oldItem: com.example.cmed_project2.model.Character, newItem: com.example.cmed_project2.model.Character): Boolean {
+        return oldItem == newItem
+    }
 }
