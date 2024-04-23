9ca8034ff0e9a572f0cdb75b753a10ea1eb45465
omi-72 <omi@techetronventures.com>
2024-04-23 15:55:19 +0600

    adding adapter and navController on CharachterFragmentList

--- app/src/main/java/com/example/cmed_project2/fragment/CharacterListFragment.kt
+++ app/src/main/java/com/example/cmed_project2/fragment/CharacterListFragment.kt
@@ -1,18 +1,42 @@
 package com.example.cmed_project2.fragment
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.cmed_project2.R
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.cmed_project2.adapter.CharacterAdapter
+import com.example.cmed_project2.databinding.FragmentCharacterListBinding
+import com.example.cmed_project2.viewmodel.CharacterViewModel
+
 class CharacterListFragment : Fragment() {
+
+    private val viewModel: CharacterViewModel by viewModels()
+    private lateinit var characterAdapter: CharacterAdapter
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_character_list, container, false)
+        val binding = FragmentCharacterListBinding.inflate(inflater, container, false)
+
+        characterAdapter.setOnItemClickListener {
+            val action = CharacterListFragmentDirections.actionCharacterListFragmentToCharacterDetailFragment()
+            findNavController().navigate(action)
+        }
+
+        characterAdapter = CharacterAdapter()
+        binding.characterRecyclerView.apply {
+            adapter = characterAdapter
+            layoutManager = LinearLayoutManager(requireContext())
+        }
+        viewModel.getCharacters().observe(viewLifecycleOwner, Observer {
+            characterAdapter.submitList(it)
+        })
+        return binding.root
     }
 
 
