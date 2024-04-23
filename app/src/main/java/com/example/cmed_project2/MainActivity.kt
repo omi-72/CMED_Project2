cabb461cf4915b177ddc0d6bf672c4dbda2a910a
omi-72 <omi@techetronventures.com>
2024-04-23 15:59:38 +0600

    set on FragmentContainer on mainActivity to navigate fragment

--- app/src/main/java/com/example/cmed_project2/MainActivity.kt
+++ app/src/main/java/com/example/cmed_project2/MainActivity.kt
@@ -2,10 +2,25 @@ package com.example.cmed_project2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.example.cmed_project2.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+    private lateinit var binding: ActivityMainBinding
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
+
+        setupActionBarWithNavController(navController)
     }
 }
