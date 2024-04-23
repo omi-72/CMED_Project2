47bbfe189b258f2fa124a60a65b1c914c4d9c9da
omi-72 <omi@techetronventures.com>
2024-04-23 14:55:02 +0600

    Create a package name model and data class name as Character

---
+++ app/src/main/java/com/example/cmed_project2/model/Character.kt
@@ -0,0 +1,8 @@
+package com.example.cmed_project2.model
+
+data class Character(
+    val firstName: String,
+    val lastName: String,
+    val mobileNumber: String,
+    val profileImage: String
+)
